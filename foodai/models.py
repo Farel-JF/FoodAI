2ee605878ab9802004ffdc066531dbcb46d70a6c
Farelito <bakoufarel@gmail.com>
2024-10-17 13:37:51 +0200

    env created

--- foodai/models.py
+++ foodai/models.py
@@ -1,38 +1,19 @@
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from foodai import db, login_manager
+from flask_login import UserMixin
 
-db = SQLAlchemy()
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(25), nullable=False)
+    lname = db.Column(db.String(25), nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(125), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    lessons = db.relationship("Lesson", backref="author", lazy=True)
+    def __repr__(self):
+        return f"User('{self.fname}', '{self.lname}', '{self.username}', '{self.email}', '{self.image_file}')"
 
-class User(db.Model):
-    __tablename__ = 'users'
-    User_id = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String(45))
-    Email = db.Column(db.String(45), unique=True, nullable=False)
-    Password = db.Column(db.String(45), nullable=False)
-    Goal = db.Column(db.Text)
-    Diet_preference = db.Column(db.Integer)
-    Created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-class Product(db.Model):
-    __tablename__ = 'products'
-    Product_id = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.String(45), nullable=False)
-    Description = db.Column(db.String(200))
-    Price = db.Column(db.Float, nullable=False)
-
-class Order(db.Model):
-    __tablename__ = 'orders'
-    Order_id = db.Column(db.Integer, primary_key=True)
-    User_id = db.Column(db.Integer, db.ForeignKey('users.User_id'), nullable=False)
-    Status = db.Column(db.String(45), default='Pending')
-    Total_amount = db.Column(db.Float)
-    Order_date = db.Column(db.DateTime, default=datetime.utcnow)
-
-class Review(db.Model):
-    __tablename__ = 'reviews'
-    Reviews_id = db.Column(db.Integer, primary_key=True)
-    User_id = db.Column(db.Integer, db.ForeignKey('users.User_id'), nullable=False)
-    Product_id = db.Column(db.Integer, db.ForeignKey('products.Product_id'), nullable=False)
-    Rating = db.Column(db.Integer)
-    Comment = db.Column(db.String(500))
-    Review_date = db.Column(db.DateTime, default=datetime.utcnow)
